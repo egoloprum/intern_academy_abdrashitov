a575b076961aa829e0d287cfb75e974519a4fb02
egoloprum <bayraa093@gmail.com>
2025-02-11 16:39:06 +0300

    input is done with error checking

--- src/shared/ui/components/Input/Input.tsx
+++ src/shared/ui/components/Input/Input.tsx
@@ -13,6 +13,8 @@ interface InputProps {
   disabled: boolean
   topLabel?: string
   bottomLabel?: string
+
+  error?: boolean
 }
 
 const Input: FC<InputProps> = ({
@@ -21,7 +23,8 @@ const Input: FC<InputProps> = ({
   rounded,  
   disabled=false, 
   topLabel='label', 
-  bottomLabel='This is a helper text'
+  bottomLabel='This is a helper text',
+  error=false
 }) => {
   const [onActive, setOnActive] = useState<boolean>(false)
   const [onHover, setOnHover] = useState<boolean>(false)
@@ -39,14 +42,16 @@ const Input: FC<InputProps> = ({
           `${size}-container`,
           `${rounded && 'border-rounded-container'}`,
           `${onActive && 'input-active-container'}`,
-          `${onHover && 'input-hover-container'}`
+          `${onHover && 'input-hover-container'}`,
+          `${error && 'input-error-container'}`
         ].join(' ')}
       >
         <Left disabled={disabled} />
         <input
           className={[
             `input ${size}-input`,
-            `${onHover && 'input-hover'}`
+            `${onHover && 'input-hover'}`,
+            `${error && 'text-error'}`,
           ].join(' ')} 
           type="text" 
           value={value}
@@ -63,7 +68,8 @@ const Input: FC<InputProps> = ({
         className={[
           'bottom-label',
           `bottom-${size}-label`,
-          `${disabled && 'text-disabled'}`
+          `${error && 'text-error'}`,
+          `${disabled && 'text-disabled'}`,
         ].join(' ')}
       >
         {bottomLabel}
