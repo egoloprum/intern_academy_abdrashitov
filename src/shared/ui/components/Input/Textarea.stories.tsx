02e24eb2989668bddd471948ef8f87904fc1440a
egoloprum <bayraa093@gmail.com>
2025-02-11 17:21:49 +0300

    textarea done

--- src/shared/ui/components/Input/Textarea.stories.tsx
+++ src/shared/ui/components/Input/Textarea.stories.tsx
@@ -11,8 +11,8 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const OnHover: Story = {
+export const OnTest: Story = {
   args: {
-    label: 'Textarea',
+    
   },
 };
--- src/shared/ui/components/Input/Textarea.tsx
+++ src/shared/ui/components/Input/Textarea.tsx
@@ -1,17 +1,69 @@
-import { FC } from 'react'
+"use client"
+
+import { FC, useState } from 'react'
+import './textarea.css'
 
 interface TextareaProps {
-  
+  size?: 'large' | 'medium' | 'small' | 'xSmall'
+  rounded?: boolean
+
+  disabled?: boolean
+  error?: boolean
+
+  topLabel?: string
+  bottomLabel?: string
 }
 
-const Textarea: FC<TextareaProps> = ({}) => {
-  return (
-    <textarea 
-      name="" 
-      id=""
-    >
+const Textarea: FC<TextareaProps> = ({
+  rounded=false,
+  size='medium',
+  disabled=false,
+  error=false, 
+  topLabel='label', 
+  bottomLabel='This is a helper text'
+}) => {
+  const [onActive, setOnActive] = useState<boolean>(false)
+  const [onHover, setOnHover] = useState<boolean>(false)
 
-    </textarea>
+  return (
+    <div 
+      className={`textarea-container`}
+    >
+      <label 
+        className={
+          `top-label top-${size}-label`
+        }
+      >
+        {size} {topLabel}
+      </label>
+      <textarea 
+        className={[
+          'textarea',
+          `${size}-textarea`,
+          `${rounded && 'border-rounded-textarea'}`,
+          `${onActive && 'textarea-active'}`,
+          `${onHover && 'textarea-hover'}`,
+          `${error && 'textarea-error'}`
+        ].join(' ')}
+        placeholder={`${size} text area`}
+        onFocus={() => setOnActive(true)}
+        onBlur={() => setOnActive(false)}
+        onMouseEnter={() => setOnHover(true)}
+        onMouseLeave={() => setOnHover(false)}
+        disabled={disabled}
+      >
+      </textarea>
+      <label 
+        className={[
+          'bottom-label', 
+          `bottom-${size}-label`,
+          `${error && 'text-error'}`,
+          `${disabled && 'text-disabled'}`,
+        ].join(' ')}
+      >
+        {bottomLabel}
+      </label>
+    </div>
   )
 }
 
