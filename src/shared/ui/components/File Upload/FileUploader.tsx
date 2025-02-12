fe71531e98475f6c92d6c8bfa587b2ccb70ecd05
egoloprum <bayraa093@gmail.com>
2025-02-12 16:24:51 +0300

    file uploader is done

--- src/shared/ui/components/File Upload/FileUploader.tsx
+++ src/shared/ui/components/File Upload/FileUploader.tsx
@@ -1,11 +1,109 @@
-import { FC } from 'react'
+"use client"
+
+import { FC, useRef, useState } from 'react'
+import './styles/fileUploader.css'
+import FileUploaderSvg from './assets/FileUploaderSvg'
+import FileCancelSvg from './assets/FileCancelSvg'
 
 interface FileUploaderProps {
-  
+  size?: 'large' | 'medium' | 'small'
+  disabled?: boolean
 }
 
-const FileUploader: FC<FileUploaderProps> = ({}) => {
-  return <div>FileUploader</div>
+const FileUploader: FC<FileUploaderProps> = ({
+  size='medium',
+  disabled=false
+}) => {
+  const [fileName, setFileName] = useState<string | null>(null)
+  const [fileSize, setFileSize] = useState<number | null>(null)
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+  const [isRotated, setIsRotated] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    if (file) {
+      setFileName(file.name)
+      setFileSize(file.size)
+      setIsLoading(true)
+
+      setTimeout(() => {
+        setIsLoading(false)
+      }, 1000)
+    } else {
+      setFileName(null)
+      setFileSize(null)
+    }
+  }
+
+  const formatFileSize = (size: number) => {
+    if (size < 1024) {
+      return `${size} bytes`
+    } else if (size < 1024 * 1024) {
+      return `${(size / 1024).toFixed(2)} KB`
+    } else {
+      return `${(size / (1024 * 1024)).toFixed(2)} MB`
+    }
+  }
+
+  const handleRemoveFile = () => {
+    setIsRotated(true)
+    setTimeout(() => {
+      setFileName(null)
+      setFileSize(null)
+      if (fileInputRef.current) {
+        fileInputRef.current.value = ''
+      }
+      setIsRotated(false)
+    }, 300)
+  }
+
+  return (
+    <div className='file-uploader-wrapper'>
+      <div 
+        className={`file-uploader-container ${disabled && 'file-disabled-container'}`}
+      >
+        <input 
+          className='file-input' 
+          type="file" 
+          onChange={handleFileChange} 
+          ref={fileInputRef}
+          disabled={disabled}
+        />
+        <div className={`loading-bar ${isLoading ? 'loading-start' : 'loading-end'} `} />
+        <FileUploaderSvg className={`${size}-file-svg`} disabled={disabled} />
+        <span 
+          className={[
+            'file-text',
+            `${size}-file-font`,
+            `${disabled && 'file-disabled-text'}`
+          ].join(' ')}
+        >
+          {fileName ? fileName : 'File Upload'}
+        </span>
+        <span 
+          className={[
+            'file-size',
+            `${size}-file-font`,
+            `${disabled && 'file-disabled-text'}`
+          ].join(' ')}
+        >
+          {fileSize ? formatFileSize(fileSize) : '3.32 MB'}
+        </span>
+        <FileCancelSvg
+          onClick={handleRemoveFile}
+          className={`${size}-file-svg ${!disabled && 'file-cancel-svg'} ${!disabled && isRotated ? 'rotate' : ''}`} 
+          disabled={disabled}
+        />
+
+      </div>
+      <label 
+        className={`file-label ${size}-file-label`}
+      >
+        This is a helper text
+      </label>
+    </div>
+  )
 }
 
 export default FileUploader
---
+++ src/shared/ui/components/File Upload/assets/FileCancelSvg.tsx
@@ -0,0 +1,17 @@
+import { ChangeEventHandler, FC, MouseEventHandler } from 'react'
+
+interface FileCancelSvgProps {
+  className?: string
+  onClick: MouseEventHandler<SVGSVGElement>
+  disabled?: boolean
+}
+
+const FileCancelSvg: FC<FileCancelSvgProps> = ({className, onClick, disabled}) => {
+  return (
+    <svg onClick={onClick} className={className} width="100%" height="100%" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" clipRule="evenodd" d="M5.80806 5.80806C6.05214 5.56398 6.44786 5.56398 6.69194 5.80806L10 9.11612L13.3081 5.80806C13.5521 5.56398 13.9479 5.56398 14.1919 5.80806C14.436 6.05214 14.436 6.44786 14.1919 6.69194L10.8839 10L14.1919 13.3081C14.436 13.5521 14.436 13.9479 14.1919 14.1919C13.9479 14.436 13.5521 14.436 13.3081 14.1919L10 10.8839L6.69194 14.1919C6.44786 14.436 6.05214 14.436 5.80806 14.1919C5.56398 13.9479 5.56398 13.5521 5.80806 13.3081L9.11612 10L5.80806 6.69194C5.56398 6.44786 5.56398 6.05214 5.80806 5.80806Z" fill="#08091C" opacity={`${disabled ? '0.36' : '1'}`}/>
+    </svg>
+  )
+}
+
+export default FileCancelSvg
